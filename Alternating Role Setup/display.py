66494c1dae5f9135f3e896ff5f88484a1f586b9b
Quan Luu <ltmquan2001@gmail.com>
2023-08-28 10:41:44 -0700

    Fix ARS/display.py

--- Alternating Role Setup/display.py
+++ Alternating Role Setup/display.py
@@ -3,7 +3,7 @@ import imageio.v2 as imageio
 import seaborn as sns
 import os
 
-def gen_gif(signal_history: list, action_history: list, ep_fn, num_iter: int, record_interval: int, duration: int, output_file: str, figsize=(15, 6), height_ratios=None):
+def gen_gif(signal_histories: list, action_histories: list, ep_fn, num_iter: int, record_interval: int, duration: int, output_file: str, figsize=(15, 6), height_ratios=None):
   """Generates a heatmap gif and saves it into 
   a GIF file
 
@@ -30,17 +30,17 @@ def gen_gif(signal_history: list, action_history: list, ep_fn, num_iter: int, re
     plt.tight_layout(pad=5)
 
     epx1.append((i+1)*record_interval)
-    epy1.append(ep_fn(signal_history[i], action_history[i]))
+    epy1.append(ep_fn(signal_histories[0][i], action_histories[0][i]))
     epx2.append((i+1)*record_interval)
-    epy2.append(ep_fn(signal_history[i], action_history[i]))
+    epy2.append(ep_fn(signal_histories[1][i], action_histories[1][i]))
 
-    sns.heatmap(signal_history[i], linewidths=0.5, linecolor="white", square=True, cbar=False, annot=True, 
+    sns.heatmap(signal_histories[0][i], linewidths=0.5, linecolor="white", square=True, cbar=False, annot=True, 
     fmt=".1f", ax=axs[0, 0])
     axs[0, 0].set_xlabel("states")
     axs[0, 0].set_ylabel("messages")
     axs[0, 0].set_title("Agent 1\'s signal weights")
 
-    sns.heatmap(action_history[i], linewidths=0.5, linecolor="white", square=True, cbar=False, annot=True, 
+    sns.heatmap(action_histories[0][i], linewidths=0.5, linecolor="white", square=True, cbar=False, annot=True, 
     fmt=".1f", ax=axs[1, 0])
     axs[1, 0].set_xlabel("actions")
     axs[1, 0].set_ylabel("messages")
@@ -51,13 +51,13 @@ def gen_gif(signal_history: list, action_history: list, ep_fn, num_iter: int, re
     axs[2, 0].set_ylabel("expected payoff")
     axs[2, 0].set_title("Expected payoff by rollout")
 
-    sns.heatmap(signal_history[i], linewidths=0.5, linecolor="white", square=True, cbar=False, annot=True, 
+    sns.heatmap(signal_histories[1][i], linewidths=0.5, linecolor="white", square=True, cbar=False, annot=True, 
     fmt=".1f", ax=axs[0, 1])
     axs[0, 1].set_xlabel("states")
     axs[0, 1].set_ylabel("messages")
     axs[0, 1].set_title("Agent 2\'s signal weights")
 
-    sns.heatmap(action_history[i], linewidths=0.5, linecolor="white", square=True, cbar=False, annot=True, 
+    sns.heatmap(action_histories[1][i], linewidths=0.5, linecolor="white", square=True, cbar=False, annot=True, 
     fmt=".1f", ax=axs[1, 1])
     axs[1, 1].set_xlabel("actions")
     axs[1, 1].set_ylabel("messages")
